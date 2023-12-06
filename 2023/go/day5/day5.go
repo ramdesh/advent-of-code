7db0c84ae9478b7fc71521ba04d66005a5e20fe3
ramdesh <rasade88@gmail.com>
2023-12-06 00:06:02 -0500

    Day 5 part 1

--- 2023/go/day5/day5.go
+++ 2023/go/day5/day5.go
@@ -16,6 +16,8 @@ func main() {
 }
 
 func Part1(input []string) int {
+	closestLocation := -1
+	seedsList := []string{}
 	seedToSoilMap := map[int]int{}
 	soilToFertilizerMap := map[int]int{}
 	fertilizerToWaterMap := map[int]int{}
@@ -23,27 +25,80 @@ func Part1(input []string) int {
 	lightToTemperatureMap := map[int]int{}
 	temperatureToHumidityMap := map[int]int{}
 	humidityToLocationMap := map[int]int{}
+	inputSize := len(input)
 	for index, line := range input {
 		if strings.Contains(line, "seeds:") {
-			seedsList := strings.Split(strings.Split(line, ":")[1], " ")
+			seedsList = strings.Split(strings.Split(line, ":")[1], " ")
 		} else if strings.Contains(line, "seed-to-soil map:") {
-			seedToSoilLines := SplitMapLines(index, input)
-
+			seedToSoilLines := SplitMapLines(index, input, inputSize)
+			seedToSoilMap = BuildAlmanacMap(seedToSoilLines)
+		} else if strings.Contains(line, "soil-to-fertilizer map:") {
+			soilToFertilizerLines := SplitMapLines(index, input, inputSize)
+			soilToFertilizerMap = BuildAlmanacMap(soilToFertilizerLines)
+		} else if strings.Contains(line, "fertilizer-to-water map:") {
+			fertilizerToWaterLines := SplitMapLines(index, input, inputSize)
+			fertilizerToWaterMap = BuildAlmanacMap(fertilizerToWaterLines)
+		} else if strings.Contains(line, "water-to-light map:") {
+			waterToLightLines := SplitMapLines(index, input, inputSize)
+			waterToLightMap = BuildAlmanacMap(waterToLightLines)
+		} else if strings.Contains(line, "light-to-temperature map:") {
+			lightToTemperatureLines := SplitMapLines(index, input, inputSize)
+			lightToTemperatureMap = BuildAlmanacMap(lightToTemperatureLines)
+		} else if strings.Contains(line, "light-to-temperature map:") {
+			lightToTemperatureLines := SplitMapLines(index, input, inputSize)
+			lightToTemperatureMap = BuildAlmanacMap(lightToTemperatureLines)
+		} else if strings.Contains(line, "temperature-to-humidity map:") {
+			temperatureToHumidityLines := SplitMapLines(index, input, inputSize)
+			temperatureToHumidityMap = BuildAlmanacMap(temperatureToHumidityLines)
+		} else if strings.Contains(line, "humidity-to-location map:") {
+			humidityToLocationLines := SplitMapLines(index, input, inputSize)
+			humidityToLocationMap = BuildAlmanacMap(humidityToLocationLines)
 		}
 
 	}
-	return 0
+	for _, seedStr := range seedsList {
+		if seedStr != "" {
+			seed, err := strconv.Atoi(seedStr)
+			if err != nil {
+				panic(err)
+			}
+			soil := CheckAndGetMapping(seed, seedToSoilMap)
+			fertilizer := CheckAndGetMapping(soil, soilToFertilizerMap)
+			water := CheckAndGetMapping(fertilizer, fertilizerToWaterMap)
+			light := CheckAndGetMapping(water, waterToLightMap)
+			temperature := CheckAndGetMapping(light, lightToTemperatureMap)
+			humidity := CheckAndGetMapping(temperature, temperatureToHumidityMap)
+			location := CheckAndGetMapping(humidity, humidityToLocationMap)
+
+			if closestLocation == -1 {
+				closestLocation = location
+			} else if location < closestLocation {
+				closestLocation = location
+			}
+		}
+	}
+	return closestLocation
 }
 
 func Part2(input []string) int {
 	return 0
 }
 
-func SplitMapLines(index int, lines []string) []string {
+func CheckAndGetMapping(key int, almanacMap map[int]int) int {
+	mappedVal := 0
+	if value, itsThere := almanacMap[key]; itsThere {
+		mappedVal = value
+	} else {
+		mappedVal = key
+	}
+	return mappedVal
+}
+
+func SplitMapLines(index int, lines []string, size int) []string {
 	outputLines := []string{}
 	// go to the next line to start building map
 	i := index + 1
-	for lines[i] != "" {
+	for i < size && lines[i] != "" {
 		outputLines = append(outputLines, lines[i])
 		i++
 	}
@@ -70,6 +125,9 @@ func BuildAlmanacMap(inputList []string) map[int]int {
 				source = almanacNum
 			} else if strIndex == 2 {
 				diff = almanacNum
+			}
+			for i := 0; i < diff; i++ {
+				almanacMap[source+i] = target + i
 			}
 
 		}
